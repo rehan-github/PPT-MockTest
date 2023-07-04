2cefbdf6bb029160c6c7a01d66cba76ea0ba3aaa
rehan-github <124896617+rehan-github@users.noreply.github.com>
2023-07-04 16:56:01 +0530

    Update question2.py

--- DS MockTest/Question 2/question2.py
+++ DS MockTest/Question 2/question2.py
@@ -1,3 +1,23 @@
+'''
+Implement a decorator function called ‘timer’ that measures the execution time of a function.
+The ‘timer’ decorator should print the time taken by the decorated function to execute.
+Use the ‘time’ module in Python to calculate the execution time.
+
+Example:
+
+import time
+
+@timer
+def my_function():
+    # Function code goes here
+    time.sleep(2)
+
+my_function()
+
+Output:
+"Execution time: 2.00123 seconds"
+'''
+
 import time
 
 def timer(func):
